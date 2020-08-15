008d0dff587ef59dd0c342035d87da197f2fac65
Emre Akı <aki.emre@icloud.com>
2020-08-15 13:51:47 +0300

    chore: add .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "printWidth": 80,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "trailingComma": "none",
+  "arrowParens": "avoid",
+  "singleQuote": false,
+  "quoteProps": "preserve",
+  "bracketSpacing": false,
+};
