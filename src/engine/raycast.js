aa0109695fd0b5ce03ce963d0a1275dfbcb09ffe
Emre Akı <aki.emre@icloud.com>
2021-12-24 18:31:26 +0300

    fix: avoid the modulo operator by using bit-wise AND
      * avoid the modulo operator by using bit-wise AND when determining
        if a given integer is odd or even

--- src/engine/raycast.js
+++ src/engine/raycast.js
@@ -591,7 +591,7 @@
               (collnX !== x1 || collnY !== y1)
             ) ++nColls;
           }
-          return nColls % 2 > 0;
+          return nColls & 1;
         },
         "rectVsRect": function(x0, y0, w0, h0, x1, y1, w1, h1) {
           return x0 + w0 > x1 && x1 + w1 > x0 && y0 + h0 > y1 && y1 + h1 > y0;
